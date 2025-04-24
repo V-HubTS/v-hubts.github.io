19c705b0fde0e3de4384b8e0cdbcff4132ff5eed
V-Hub.TS <a583709.v@gmail.com>
2025-04-23 20:22:45 -0700

    Create vts.go

---
+++ vhubtsv12/vts.go
@@ -0,0 +1,106 @@
+package vts
+
+import (
+    "bufio"
+    "encoding/json"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+    "strings"
+    "time"
+
+    u "github.com/jolav/codetabs/_utils"
+)
+
+type vts struct {
+    quest string
+}
+
+func Router(w http.ResponseWriter, r *http.Request) {
+    params := strings.Split(strings.ToLower(r.URL.Path), "/")
+    path := params[1:len(params)]
+    if path[len(path)-1] == "" { // remove last empty slot after /
+        path = path[:len(path)-1]
+    }
+
+    if len(path) < 2 || path[0] != "v1" {
+        u.BadRequest(w, r)
+        return
+    }
+
+    p := newVTS(false)
+    r.ParseForm()
+    p.quest = r.Form.Get("quest")
+
+    if r.Method != http.MethodGet {
+        log.Printf("%s -> %s", r.Method, p.quest)
+        u.BadRequest(w, r)
+        return
+    }
+
+    if p.quest == "" || len(path) != 2 {
+        u.BadRequest(w, r)
+        return
+    }
+    p.doVTSRequest(w, r)
+}
+
+func (p *vts) doVTSRequest(w http.ResponseWriter, r *http.Request) {
+    p.quest = "http://" + u.RemoveProtocolFromURL(p.quest)
+    var data interface{}
+    var netClient = &http.Client{
+        Timeout: time.Second * 10,
+    }
+    resp, err := netClient.Get(p.quest)
+    if err != nil {
+        log.Printf("Error VTS => %s", err)
+        msg := fmt.Sprintf("%s is not a valid resource", p.quest)
+        u.ErrorResponse(w, msg)
+        return
+    }
+    defer resp.Body.Close()
+
+    contentType := ""
+    if len(resp.Header["Content-Type"]) > 0 {
+        contentType = resp.Header["Content-Type"][0]
+    }
+
+    if strings.Contains(contentType, "application/json") {
+        json.NewDecoder(resp.Body).Decode(&data)
+        if data != nil {
+            w.Header().Set("Content-Type", "application/json")
+            u.SendJSONToClient(w, data, 200)
+            return
+        }
+    }
+
+    if strings.Contains(contentType, "application/xml") {
+        u.SendXMLToClient(w, data, 200)
+        return
+    }
+
+    if strings.Contains(contentType, "text/") {
+        w.Header().Add("Content-type", "text/plain")
+    }
+
+    reader := bufio.NewReader(resp.Body)
+    for {
+        line, err := reader.ReadBytes('\n')
+        if err != nil {
+            if err != io.EOF {
+                log.Printf("Error VTS-2: %s => %v\n", p.quest, err)
+            }
+            w.Write([]byte(fmt.Sprintf("%v", string(line))))
+            return
+        }
+        w.Write([]byte(fmt.Sprintf("%v", string(line))))
+    }
+}
+
+func newVTS(test bool) vts {
+    p := vts{
+        quest: "",
+    }
+    return p
+}
